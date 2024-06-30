1c86294ba807101498648f4af3b443dbb6de1f1f
JiXiangChua <jixiangchua2697@gmail.com>
2024-06-30 18:17:24 +0800

    first commit

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTCalendarModule.h
+//  RNNativeCalendarApp
+//
+//  Created by JI XIANG on 30/6/24.
+//
+
+#ifndef RCTCalendarModule_h
+#define RCTCalendarModule_h
+
+
+#endif /* RCTCalendarModule_h */
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTCalendarModule.m
+//  RNNativeCalendarApp
+//
+//  Created by JI XIANG on 30/6/24.
+//
+
+#import <Foundation/Foundation.h>
